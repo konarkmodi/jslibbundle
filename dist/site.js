e14816c597856df34267e2bf82213ab0f7cfc0c7
Noufal Ibrahim <noufal@nibrahim.net.in>
2020-09-17 15:25:16 +0530

    Example custom component

--- dist/site.js
+++ dist/site.js
@@ -4,8 +4,21 @@ d = genskill.init();
 window.onload = function() {
     console.log("Here !", d.Vue);
 
+    var component_a = {
+        data: function () {
+            return {
+                count: 0
+            }
+        },
+        template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+    };
+ 
+
     var app = new d.Vue({
         el: '#app',
+        components: {
+            'compa': component_a
+        },
         data: {
             message: 'Hello Vue!'
         }
